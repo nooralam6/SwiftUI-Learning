3a0e19c73f4cf21d310f733d29482aad7456a988
Nooralam Shaikh <shkhnooralam6@gmail.com>
2022-02-22 10:41:22 +0530

    Lecture 4

---
+++ Memorize/Memorize/MemorizeApp.swift
@@ -0,0 +1,19 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Nooralam Shaikh on 14/02/22.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    let game = EmojiMemoryGame()
+    
+    var body: some Scene {
+        WindowGroup {
+            ContentView(viewModel: game)
+        }
+    }
+}
